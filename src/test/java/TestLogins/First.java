61f418ef6422a047ad3c2801b50356eeff6b39d4
gitandjenrepo <jhansi.nagadasari09@gmail.com>
2025-04-28 23:45:24 -0500

    first

---
+++ src/test/java/TestLogins/First.java
@@ -0,0 +1,17 @@
+package TestLogins;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+
+public class First {
+	
+	@Test
+	public void testone() {
+		System.out.println("Print testfour");
+		
+		System.out.println("Jhansi modified code here to see git changes");
+	}
+
+}
+
---
+++ src/test/java/TestLogins/TestTwo.java
@@ -0,0 +1,14 @@
+package TestLogins;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TestTwo {
+	
+	@Test
+	public void testtwo() {
+		
+		System.out.println("Print test ttwo");
+	}
+
+}
